43d6fd57a43946ec55a5fc983db1db9ec584b1a1
Run-J <jirun040403@hotmail.com>
2025-07-03 23:04:14 +0800

    Initial Commit (Server can running now)
        1: Initialize the Repo toolchain enviroment.
        2: Reset the original react-native template, and add my test template.
        3: Add the images.

---
+++ app/index.tsx
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello StickerSmash.</Text>
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#25292e",
+  },
+  text: {
+    color: "white"
+  }
+})
