4576c08b1602cd66768bcce136705d95a1895750
Run-J <jirun040403@hotmail.com>
2025-07-05 00:36:16 +0800

    Add another page and navi bar
    1: Add a new screen to the stack
    2: Add not-found route
    3: Add a buttom tab navigator (tabs)
    4: Update bottom tab navi appearance
    5: Add status bar feature

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,55 @@
+import { Ionicons } from '@expo/vector-icons';
+import { Tabs } from "expo-router";
+
+export default function TabsLayout() {
+  return (
+    <Tabs 
+      screenOptions={{
+        tabBarActiveTintColor: "#ffd33d",
+        headerStyle: {
+          backgroundColor: "#25292e",
+        },
+        headerShadowVisible: false,
+        headerTintColor: "#fff",
+        tabBarStyle: {
+          backgroundColor: "#25292e",
+        }
+      }}
+    >
+
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Sticker Smash",
+          tabBarIcon: ({ focused, color }) => (
+            <Ionicons 
+              name={focused ? "home-sharp" : "home-outline"}
+              color={color}
+              size={24}
+            />
+          )
+        }}
+      />
+      <Tabs.Screen
+        name="about"
+        options={{
+          title: 'About',
+          tabBarIcon: ({ focused, color }) => (
+            <Ionicons
+              name={focused ? "information-circle" : "information-circle-outline"}
+              color={color}
+              size={24}
+            />
+          )
+        }}
+      />
+      <Tabs.Screen
+        name="+not-found"
+        options={{
+
+        }}
+      />
+    </Tabs>
+  
+  );
+}
